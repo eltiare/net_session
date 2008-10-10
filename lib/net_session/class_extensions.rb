132e07ad06fb2ba36ba8d6d5c2c9f5a7901abdbb
Jeremy Nicoll <eltiare@Chubby.(none)>
2008-10-10 11:33:27 -0600

    Fixed blank? methods definitions to actually check for pre-definitions.

--- lib/net_session/class_extensions.rb
+++ lib/net_session/class_extensions.rb
@@ -1,5 +1,5 @@
 # The blank classes
-class NilClass; def blank?; true; end; end unless NilClass.respond_to?(:blank?)
-class String; def blank?; match(/^\s*$/) ? true: false; end; end unless String.respond_to?(:blank?)
-[Hash, Array].each { |klass| klass.class_eval {def blank?; size == 0; end} unless klass.respond_to?(:blank?)}
-class Object; def blank?; false; end; end unless Object.respond_to?(:blank?)
+class NilClass; def blank?; true; end; end unless nil.respond_to?(:blank?)
+class String; def blank?; match(/^\s*$/) ? true: false; end; end unless ''.respond_to?(:blank?)
+[Hash, Array].each { |klass| klass.class_eval {def blank?; size == 0; end} unless klass.new.respond_to?(:blank?)}
+class Object; def blank?; false; end; end unless Object.new.respond_to?(:blank?)
